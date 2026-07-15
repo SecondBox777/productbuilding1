02ceb64c1865e8073f0bab5dbc288b49a7028639
SecondBox777 <pepermint916@gmail.com>
2026-07-15 06:15:21 +0000

    Add Naver site verification

--- worker.js
+++ worker.js
@@ -1,12 +1,16 @@
-const VERIFICATION_PATH = "/google8e1ad6d7f4037882.html";
-const VERIFICATION_BODY = "google-site-verification: google8e1ad6d7f4037882.html";
+const VERIFICATION_RESPONSES = {
+  "/google8e1ad6d7f4037882.html":
+    "google-site-verification: google8e1ad6d7f4037882.html",
+  "/naver8c57345239d00813fcb9a96ada5cffba.html":
+    "naver-site-verification: naver8c57345239d00813fcb9a96ada5cffba.html",
+};
 
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === VERIFICATION_PATH) {
-      return new Response(VERIFICATION_BODY, {
+    if (VERIFICATION_RESPONSES[url.pathname]) {
+      return new Response(VERIFICATION_RESPONSES[url.pathname], {
         status: 200,
         headers: {
           "Content-Type": "text/html; charset=UTF-8",
